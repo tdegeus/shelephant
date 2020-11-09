060ca182332c3ce4c70fbf22b53453aa4d87b6d2
Tom de Geus <tom@geus.me>
2020-11-09 21:16:42 +0100

    Writing script to dump files

--- yaml_cli/cli/yaml_dump.py
+++ yaml_cli/cli/yaml_dump.py
@@ -5,12 +5,16 @@ Usage:
     yaml_dump [options] <file>...
 
 Options:
-    -o, --output=N  Output YAML-file. [default: dump.yaml]
-    -p, --path=N    Path within the YAML-file separated by "/". [default: /]
-    -c, --command   Interpret the input as a command.
-    -f, --force     Force overwrite
-    -h, --help      Show help.
-        --version   Show version.
+    -o, --output=N      Output YAML-file. [default: dump.yaml]
+    -p, --path=N        Path within the YAML-file separated by "/". [default: /]
+    -c, --command       Interpret the input as a command.
+    -f, --force         Force overwrite
+    -n, --normpath      Normalize file-paths.
+    -r, --relpath       Convert to a relative file-path
+    -a, --abspath       Convert file-paths to absolute paths.
+        --prefix=N      Add a prefix to all file-paths.
+    -h, --help          Show help.
+        --version       Show version.
 
 (c - MIT) T.W.J. de Geus | tom@geus.me | www.geus.me | github.com/tdegeus/yaml_cli
 '''
@@ -22,6 +26,7 @@ import sys
 import yaml
 import operator
 import functools
+import subprocess
 
 from .. import __version__
 
@@ -66,10 +71,21 @@ Main program.
     path = list(filter(None, args['--path'].split('/')))
 
     if len(path) > 0:
-        data = {}
-        functools.reduce(operator.getitem, path, data).update(files)
+        data = functools.reduce(lambda x, y: {y: x}, path[:-1], {path[-1]: files})
     else:
         data = files
+
+    if args['--normpath']:
+        files = [os.path.normpath(file) for file in files]
+
+    if args['--relpath']:
+        files = [os.path.relpath(file) for file in files]
+
+    if args['--abspath']:
+        files = [os.path.abspath(file) for file in files]
+
+    if args['--prefix']:
+        files = [os.path.join(args['--prefix'], file) for file in files]
 
     YamlDump(args['--output'], data, args['--force'])
 
