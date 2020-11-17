de4db8a7a6437068cf0cb60264f052cf4a1ec77f
Tom de Geus <tom@geus.me>
2020-11-17 12:27:05 +0100

    Fixing

--- shelephant/cli/shelephant_remote.py
+++ shelephant/cli/shelephant_remote.py
@@ -21,7 +21,7 @@ Options:
 (c - MIT) T.W.J. de Geus | tom@geus.me | www.geus.me | github.com/tdegeus/shelephant
 '''
 
-import docopt
+import argparse
 import click
 import os
 import sys
@@ -39,7 +39,40 @@ from . import CopyFromRemote
 
 def main():
 
-    args = docopt.docopt(__doc__, version=__version__)
+    # Parse command-line arguments
+
+    class Parser(argparse.ArgumentParser):
+
+        def print_help(self):
+            print(__doc__)
+
+    parser = Parser()
+    parser.add_argument('-o', '--output', required=False, default='shelephant_remote.yaml')
+    parser.add_argument(      '--force', required=False, action='store_true')
+    parser.add_argument(      '--host', required=False, default=None)
+    parser.add_argument('-p', '--prefix', required=False, default=None)
+    parser.add_argument('-f', '--files', required=False, default=None, nargs='?', const='shelephant_dump.yaml')
+    parser.add_argument('-c', '--checksum', required=False, default=None, nargs='?', const='shelephant_checksum.yaml')
+    parser.add_argument(      '--files-key', required=False, default='/')
+    parser.add_argument(      '--checksum-key', required=False, default='/')
+    parser.add_argument('-i', '--ignore', required=False, action='store_true')
+    parser.add_argument(      '--verbose', required=False, action='store_true')
+    parser.add_argument('-v', '--version', action='version', version=__version__)
+
+    p = parser.parse_args()
+
+    args = {
+        '--output' : p.output,
+        '--force' : p.force,
+        '--host' : p.host,
+        '--prefix' : p.prefix,
+        '--files' : p.files,
+        '--checksum' : p.checksum,
+        '--files-key' : p.files_key,
+        '--checksum-key' : p.checksum_key,
+        '--ignore' : p.ignore,
+        '--verbose' : p.verbose,
+    }
 
     # Extract basic information from command-line arguments
 
