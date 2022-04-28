76f7087b722815ecfd5d6e1feb9fe33394e12810
Tom de Geus <tom@geus.me>
2022-04-28 09:59:41 +0200

    Various cleanup

--- setup.py
+++ setup.py
@@ -1,35 +1,40 @@
-"""
-Module installation.
-"""
+from pathlib import Path
+
 from setuptools import find_packages
 from setuptools import setup
 
+project_name = "shelephant"
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
-    name="shelephant",
+    name=project_name,
     license="MIT",
     author="Tom de Geus",
     author_email="tom@geus.me",
     description="YAML based shell commands",
-    long_description="YAML based shell commands",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     keywords="YAML, Bash",
-    url="https://github.com/tdegeus/shelephant",
+    url=f"https://github.com/tdegeus/{project_name:s}",
     packages=find_packages(),
-    use_scm_version={"write_to": "shelephant/_version.py"},
+    use_scm_version={"write_to": f"{project_name}/_version.py"},
     setup_requires=["setuptools_scm"],
     install_requires=["click", "pyyaml", "mergedeep", "numpy"],
     entry_points={
         "console_scripts": [
-            "shelephant_checksum = shelephant.cli.shelephant_checksum:main",
-            "shelephant_dump = shelephant.cli.shelephant_dump:main",
-            "shelephant_extract = shelephant.cli.shelephant_extract:main",
-            "shelephant_get = shelephant.cli.shelephant_get:main",
-            "shelephant_merge = shelephant.cli.shelephant_merge:main",
-            "shelephant_parse = shelephant.cli.shelephant_parse:main",
-            "shelephant_hostinfo = shelephant.cli.shelephant_hostinfo:main",
-            "shelephant_cp = shelephant.cli.shelephant_cp:main",
-            "shelephant_mv = shelephant.cli.shelephant_mv:main",
-            "shelephant_rm = shelephant.cli.shelephant_rm:main",
-            "shelephant_send = shelephant.cli.shelephant_send:main",
+            f"shelephant_checksum = {project_name}.cli.shelephant_checksum:main",
+            f"shelephant_dump = {project_name}.cli.shelephant_dump:main",
+            f"shelephant_extract = {project_name}.cli.shelephant_extract:main",
+            f"shelephant_get = {project_name}.cli.shelephant_get:main",
+            f"shelephant_merge = {project_name}.cli.shelephant_merge:main",
+            f"shelephant_parse = {project_name}.cli.shelephant_parse:main",
+            f"shelephant_hostinfo = {project_name}.cli.shelephant_hostinfo:main",
+            f"shelephant_cp = {project_name}.cli.shelephant_cp:main",
+            f"shelephant_mv = {project_name}.cli.shelephant_mv:main",
+            f"shelephant_rm = {project_name}.cli.shelephant_rm:main",
+            f"shelephant_send = {project_name}.cli.shelephant_send:main",
         ]
     },
 )
